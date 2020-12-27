b80552c4b8d5d59d650b90d566b5e8e5368548cc
LunarzFR <valentino00762@gmail.com>
2020-12-28 00:40:29 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Valentin Harny on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
