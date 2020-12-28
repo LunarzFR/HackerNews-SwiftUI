5721c6f4c0e79e7e9749d085c99d4fc04adc224e
LunarzFR <valentino00762@gmail.com>
2020-12-28 01:50:28 +0100

    project finished

---
+++ HackerNews/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Valentin Harny on 28/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                            .bold()
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationTitle("Hacker News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Valentin Harny on 28/12/2020.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
